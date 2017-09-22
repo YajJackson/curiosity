be50f4cfea95f083f9b9cbd385300c567d37d19b
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-09-22 00:03:45 -0400

    added background image, needs to be adjusted for mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 // import {BrowserRouter, Route, Link } from 'react-router-dom'
-import {Grid, Row, Col} from 'react-bootstrap'
+import {Grid} from 'react-bootstrap'
 
-import NavHeader from './components/NavHeader'
-import CoursePage from './components/courses/CoursePage'
-import StudyPage from './components/study/StudyPage'
+// import NavHeader from './components/NavHeader'
+// import CoursePage from './components/courses/CoursePage'
+// import StudyPage from './components/study/StudyPage'
 
 class App extends Component {
   render() {
